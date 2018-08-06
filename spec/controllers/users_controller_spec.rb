7bcdcf9bcb10958b1cbefade5ad5558734812e8b
Hunter Brown <hunterstokes@me.com>
2018-08-06 10:14:21 -0700

    User Authentication tests complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,31 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-    describe "User API" do
-        it "can show all profiles" do
-            User.create(name: 'Hunter', email: 'hunterstokes@me.com', password: 'examplepass')
-            get :index
-            json = JSON.parse(response.body)
-            expect(response).to have_http_status(200)
-            expect(json.length).to eq 1
-        end
 
-        # TODO gets the tests to work.
-        it "creates a new user" do
-            user_params = {
-                user: {
-                    name: "Hunter",
-                    email: "hunterstokes@me.com",
-                    password: "examplepass"
-                }
-            }
-            post :create, params: user_params
-            expect(response).to have_http_status(200)
-            new_user = User.first
-            expect(new_user.name).to eq("Hunter")
-            expect(new_user.email).to eq("hunterstokes@me.com")
-        end
-        
-
-    end
 end
---
+++ spec/controllers/yelp_controller_spec.rb
@@ -0,0 +1,13 @@
+
+# require 'rails_helper'
+#
+# RSpec.describe YelpController, type: :controller do
+#
+#   describe "GET #request" do
+#     it "returns http success" do
+#       get :request
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+#
+# end
