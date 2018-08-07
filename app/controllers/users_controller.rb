246254fab11633281f9d396517cb9513174e6e19
Brian Harrington <brianharrington2014@gmail.com>
2018-08-07 13:22:05 -0700

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,6 @@ class UsersController < ApplicationController
     end
 
     def create
-<<<<<<< HEAD
-            user = User.create(user_params)
-                if user.valid?
-            render json: user
-        else
-render json: user.errors, status: :unprocessable_entity
-=======
         user = User.new(user_params)
         if user.save
             token = Knock::AuthToken.new(payload: { sub: user.id }).token
@@ -24,7 +17,6 @@ render json: user.errors, status: :unprocessable_entity
             render json: payload, status: 201
         else
             render json: {errors: user.errors}, status: 422
->>>>>>> master
         end
     end
 
