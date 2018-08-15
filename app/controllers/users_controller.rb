4e50c69e22c3cd46024983083764499c5fe089f0
Brian Harrington <brianharrington2014@gmail.com>
2018-08-15 10:27:33 -0700

    debugging auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,12 @@ class UsersController < ApplicationController
         user = User.new(user_params)
         if user.save
             token = Knock::AuthToken.new(payload: { sub: user.id }).token
+            puts "token #{token}"
             payload = {
                 user: user,
                 jwt: token
             }
+            puts "#{payload}"
             render json: payload, status: 201
         else
             render json: {errors: user.errors}, status: 422
